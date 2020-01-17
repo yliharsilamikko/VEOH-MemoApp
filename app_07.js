f23fa2cdf70524a81818239352c8cd5c5dd06533
Ylihärsilä <Mikko.Yliharsila@seamk.fi>
2020-01-17 14:32:08 +0200

    modify 07.js

--- app_07.js
+++ app_07.js
@@ -8,6 +8,7 @@ const server = http.createServer((req, res) => {
     const method = req.method;
     console.log(`HTTP request received: url=${url} , method=${method}`);
 
+
     if (url === '/') {
         res.write(`
         <html>
