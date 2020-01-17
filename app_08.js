4ee03aee6da08c2c3ef7e6aa9e5ee17230a3d3e2
Ylihärsilä <Mikko.Yliharsila@seamk.fi>
2020-01-17 15:16:20 +0200

    commit

---
+++ app_08.js
@@ -0,0 +1,102 @@
+const http = require('http');
+const fs = require('fs');
+
+const notes = [];
+
+const server = http.createServer((req, res) => {
+    const url = req.url;
+    const method = req.method;
+    console.log(`HTTP request received: url=${url} , method=${method}`);
+
+    // MemoApp app_08.js
+
+    // - Lisää Delete-nappi, jokaiselle notelle
+    // ==> Lisää forEach loopissa formi, jossa delete nappi
+    // - input type="hidden" value="${index}"
+
+    if (url === '/') {
+        res.write(`
+        <html>
+        <head><title>MemoApp</title></head>
+        <body>`);
+
+        notes.forEach((value, index) => {
+            res.write(`
+            <div>note:${value}, index: ${index}
+            <form action="delete-note" method="POST">
+                <input type="hidden" name="index", value="${index}">
+                <button type="submit">Delete</button>
+            </form>
+            </div>
+            `);
+        });
+
+        res.write(`
+            <form action="add-note" method="POST">
+                <input type="text" name="note">
+                <button type="submit">Add note</button>
+            </form>
+            <form action="delete-note" method="POST">
+                <input type="number" name="index">
+                <button type="submit">Delete note</button>
+            </form>
+        </body>
+        </html>
+        `);
+        res.statusCode = 200; //Ok
+        res.end();
+        return;
+    } else if (url === '/add-note') {
+        console.log('/add-note');
+        const chunks = [];
+        req.on('data', (chunk) => {
+            chunks.push(chunk);
+        });
+
+        req.on('end', () => {
+            const body = Buffer.concat(chunks).toString();
+            const note = body.split('=')[1];
+            notes.push(note);
+            res.statusCode = 303; //Redirect
+            res.setHeader('Location', '/');
+            res.end();
+        });
+        return;
+    } else if (url === '/delete-note') {
+        console.log('/delete-note');
+        const chunks = [];
+        req.on('data', (chunk) => {
+            chunks.push(chunk);
+        });
+
+        req.on('end', () => {
+            const body = Buffer.concat(chunks).toString();
+            const index = body.split('=')[1];
+            notes.splice(index, 1);
+            res.statusCode = 303; //Redirect
+            res.setHeader('Location', '/');
+            res.end();
+        });
+        return;
+    } else if (url === '/favicon.ico') {
+        fs.readFile('./favicon.ico', (err, data) => {
+            res.write(data);
+            res.end();
+        });
+        return;
+    }
+
+    console.log(`${url} not found`);
+    res.write(`
+    <html>
+        <head><title>MemoApp - 404</title></head>
+        <body>
+            <h1>404 - page not found</h1>
+        </body>
+    </html>`);
+    res.statusCode = 404; //Not Found
+    res.end();
+
+});
+
+server.listen(8080);
